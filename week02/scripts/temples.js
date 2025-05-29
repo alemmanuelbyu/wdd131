77c2201275cd322929692845f6791f3d48bb2c98
alemmanuelbyu <eirebamiolusola@byupathway.edu>
2025-05-29 11:35:35 +0100

    Temple album

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -3,3 +3,10 @@ let currentYearElement = document.getElementById("currentyear");
 currentYearElement.textContent = currentYear;
 let lastModifiedElement = document.getElementById("lastModified");
 lastModifiedElement.textContent = `last modified: ${document.lastModified}`;
+
+const hamButton = document.querySelector("#menu")
+const navigation = document.querySelector(".navigation")
+hamButton.addEventListener("click", () => {
+    hamButton.classList.toggle("open")
+    navigation.classList.toggle("open")
+});
