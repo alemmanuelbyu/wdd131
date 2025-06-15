e3604e9b5ac90806eff40351ec9b54856ca78012
alemmanuelbyu <eirebamiolusola@byupathway.edu>
2025-06-15 17:37:28 +0100

    form added

---
+++ scripts/form.js
@@ -0,0 +1,20 @@
+const products = [
+    { id: "latte", name: "Latte", averagerating: 4.5 },
+    { id: "cappuccino", name: "Cappuccino", averagerating: 4.7 },
+    { id: "espresso", name: "Espresso", averagerating: 4.2 },
+    { id: "mocha", name: "Mocha", averagerating: 4.8 },
+    { id: "drip-coffee", name: "Drip Coffee", averagerating: 4.0 }
+];
+
+const productNameSelect = document.getElementById('Productname');
+
+products.forEach(product => {
+    const option = document.createElement('option');
+    option.value = product.id;
+    option.textContent = product.name;
+    productNameSelect.appendChild(option);
+});
+const reviewCount = localStorage.getItem('reviewCount') || 0;
+localStorage.setItem('reviewCount', parseInt(reviewCount) + 1);
+document.getElementById('review-count').innerText = `Review Count: ${localStorage.getItem('reviewCount')}`;
+
